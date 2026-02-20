f2e0d8ed2d9ecfebc80cae63f17799dc75a08815
imeshpahasara <imeshpahasara95@gmail.com>
2026-02-20 15:14:06 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 const settings = {
   packname: 'Knight Bot',
-  author: '‎',
+  author: '‎.',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
+  botOwner: 'Professor', // imesh pahasara
   ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
